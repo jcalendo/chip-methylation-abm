30f77cc4c82f672fa92830a30a6e030bbaa4304d
jcalendo <jcalendo@gmail.com>
2026-02-25 16:34:04 -0500

    output test results to gzipped csv

--- main.py
+++ main.py
@@ -2,14 +2,36 @@
 Run a test simulation. Save results in a gzipped compressed csv file. I'd rather plot data in R...
 """
 
+import gzip
+
+from datetime import datetime
 from model import AgingModel
+
+ 
+CELLS = 1_000
+CPGS = 10
+GENES = 1_000
+P_METH = 1e-3
+P_UNMETH = 1e-6
+STEPS = 100
 
 
 def main():
-    model = AgingModel(n_agents=10_000, n_cpgs=10, n_genes=1000, p_meth=1e-3, p_unmeth=1e-7)
-    model.run_for(100)
+    model = AgingModel(n_agents=CELLS, n_cpgs=CPGS, n_genes=GENES, p_meth=P_METH, p_unmeth=P_UNMETH)
+    model.run_for(STEPS)
+
     df = model.datacollector.get_agent_vars_dataframe()
-    df.to_csv("results.csv.gz", compression="gzip")
+
+    # Write agent-level results to compressed csv file
+    # Add a comment line to the csv header indicating run parameters
+    fname = f"results/{datetime.now().strftime("%Y%m%d-%H%M%S")}_results.csv.gz"
+    comment = f"# Cells: {CELLS}, CpGs: {CPGS}, Genes: {GENES}, P_meth: {P_METH}, P_unmeth: {P_UNMETH}\n"
+    with gzip.open(fname, "wt") as outfile:
+        outfile.write(comment)
+
+    with gzip.open(fname, "at") as outfile:
+        df.to_csv(outfile)
+
 
 if __name__ == "__main__":
     main()
